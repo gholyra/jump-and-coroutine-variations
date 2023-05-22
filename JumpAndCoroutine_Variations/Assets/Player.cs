c653530eee6ed46ea5e57cf5d5f693012cf7fa90
gholyra <tiago.mlyra@gmail.com>
2023-05-22 07:42:41 -0300

    1º Commit do Projeto
    Adições no script com a corotina

---
+++ JumpAndCoroutine_Variations/Assets/Player.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private IEnumerator DestroyPlayer()
+    {
+        Color c = GetComponent<Renderer>().material.color;
+        for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
+        {
+            c.a = alpha;
+            GetComponent<Renderer>().material.color = c;
+            yield return new WaitForSeconds(.1f);
+        }
+        Destroy(gameObject);
+    }
+
+}
